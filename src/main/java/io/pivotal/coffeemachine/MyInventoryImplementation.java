e5253e0324faaed27e65265235b2ad82840375a5
Tanmay Sharma <sharma.tanmaysharma@gmail.com>
2019-05-02 01:11:36 -0400

    Testing Commit

---
+++ src/main/java/io/pivotal/coffeemachine/MyInventoryImplementation.java
@@ -0,0 +1,17 @@
+package io.pivotal.coffeemachine;
+
+import java.util.Map;
+
+public class MyInventoryImplementation implements Inventory{
+
+	public Map<String, Integer> getIngredients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deduct(String name, Integer amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
